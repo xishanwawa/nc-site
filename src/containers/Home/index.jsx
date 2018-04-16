db94296825c765010be7ea2d4870f245c4e6d2d4
yangtmm <yangtmm@yonyou.com>
2018-04-16 16:40:30 +0800

    10分钟测试表单本地和服务器新增删除

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -6,33 +6,80 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 //导入UI组件
-import Counter     from "components/Home/Counter"
+import Counter from "components/Home/Counter"
 
 //导入action方法
-import {onIncrement, onDecrement} from "actions/home"
+import { onIncrement, onDecrement } from "actions/home"
 
 class Home extends React.Component {
 
-    constructor(props) {
-      super(props)
-    }
+  constructor(props) {
+    super(props)
+    let data = [
+      {
+        name: "ytm1",
+      }, {
+        name: "ytm2",
+      }, {
+        name: "ytm3"
+      }
+    ]
+    data = data.map((item, index) => {
+      return Object.assign({}, item, { localIndex: index })
+    })
 
-    componentDidMount() {
+    this.state = {
+      data
     }
+  }
 
-    render() {
-    	const { $$state } = this.props;
-      let countValue = $$state.get("val");
-      return (
-      	<div>
-            <Counter
-              value={ countValue }
-              onIncrement={ this.props.onIncrement.bind(this) }
-              onDecrement={ this.props.onDecrement.bind(this) }
-            />
-        </div>
-      )
-    }
+  componentDidMount() {
+  }
+
+  onDelete = (localIndex) => {
+    let localData = this.state.data;
+    if(localData[localIndex]["isAdd"]){
+      localData.splice(localIndex, 1)
+    }else{
+      localData[localIndex]["delete"] = 1;
+    };
+
+    this.setState({data: localData});
+  }
+
+  addItem = () => {
+    let localData = this.state.data;
+    localData.push({name:"add", localIndex: localData.length, isAdd: 1})
+    this.setState({data: localData});
+  }
+
+  submit =()=>{
+    let localData = this.state.data;
+
+    localData = localData.map((item=>{
+      return item
+    }))
+    console.log(localData);
+  }
+
+  render() {
+
+    let renderUiData = this.state.data.filter((item) => {
+      return item.delete != 1;
+    });
+
+    let nodeUi = renderUiData.map((item) => {
+      return <div>{item.name}<button onClick={() => {this.onDelete(item.localIndex)}}>{"删除"}</button></div>
+    });
+
+    return (
+      <div>
+        <button onClick={() => {this.addItem()}}>{"新增"}</button>
+        {nodeUi}
+        <button onClick={() => {this.submit()}}>{"提交"}</button>
+      </div>
+    )
+  }
 }
 
 
